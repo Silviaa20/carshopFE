eb9baf653acafb784d5927e50b8001d2e1f80075
vincenzocontrader <v.didonato@contrader.it>
2024-03-04 13:03:56 +0100

    modifichine

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {
         path: '', //nell'url
-        title: "Home prodotto",
-        loadComponent: () =>import('./prodotto/prodotto/prodotto.component'),
-      },
-
-
-
-
-
-
-
-
-
-
+        component: LoginComponent, // Specifica il componente da caricare
+        data: { title: 'Home prodotto' } // Puoi utilizzare 'data' per passare eventuali dati aggiuntivi alla rotta
+    }
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,1 +1,28 @@
+import {Component} from '@angular/core';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
 
+
+/** @title Form field with error messages */
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, CommonModule],
+  
+})
+export class LoginComponent {
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+}
